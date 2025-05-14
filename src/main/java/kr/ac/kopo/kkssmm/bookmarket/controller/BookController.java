adb67fc020ded75d209c59acac2859ccb740bb68
김승민 <tmdals6102@gmail.com>
2025-05-14 11:32:22 +0900

    BookMarket 9장 유효성 검사 Bean Validation

--- src/main/java/kr/ac/kopo/kkssmm/bookmarket/controller/BookController.java
+++ src/main/java/kr/ac/kopo/kkssmm/bookmarket/controller/BookController.java
@@ -1,6 +1,7 @@
 package kr.ac.kopo.kkssmm.bookmarket.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import kr.ac.kopo.kkssmm.bookmarket.domain.Book;
 import kr.ac.kopo.kkssmm.bookmarket.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -61,12 +63,16 @@ public class BookController {
     }
 
     @GetMapping("/add")
-    public String requestAddBookForm() {
-        return "addBook";
+    public String requestAddBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
     }
 
     @PostMapping("/add")
-    public String requestSubmitNewBook(@ModelAttribute("book") Book book) {
+    public String requestSubmitNewBook(@Valid @ModelAttribute("book") Book book, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "addbook";
+        }
         MultipartFile multipartFile = book.getBookImage();
         String saveName = multipartFile.getOriginalFilename();
         File saveFile = new File(fileDir + saveName);
